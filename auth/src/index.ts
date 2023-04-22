a8aa4878574365e28000cea800892ead8be7f754
Mendel Mendelovich <meni905@gmail.com>
2023-04-22 23:49:02 +0300

    Revert "Add Auth service error handling and validation"
    This reverts commit 2aab7a3341ff4e65295b3daf051e11660d79a790.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,26 +1,11 @@
 import express, { json } from "express";
-import "express-async-errors";
-
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
 
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", async () => {
-  throw new NotFoundError();
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("test");
 });
-
-app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
